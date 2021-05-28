56d2a8b002a6fc791633c287dee3eacbb20e888d
sounaksaha <sounaksaha100@gmail.com>
2021-05-28 21:46:59 +0530

    Dependency added

--- app/src/main/java/com/codingwithme/hot_news/MainActivity.kt
+++ app/src/main/java/com/codingwithme/hot_news/MainActivity.kt
@@ -2,10 +2,18 @@ package com.codingwithme.hot_news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
+
+
+
