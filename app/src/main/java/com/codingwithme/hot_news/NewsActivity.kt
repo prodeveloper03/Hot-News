66e0b027a25561303cd6ac8e5dad15b4afb62323
sounak saha <sounaksaha100@gmail.com>
2021-06-14 13:14:26 +0530

    refactored mainactivity to newsactivity

--- app/src/main/java/com/codingwithme/hot_news/NewsActivity.kt
+++ app/src/main/java/com/codingwithme/hot_news/NewsActivity.kt
@@ -2,14 +2,12 @@ package com.codingwithme.hot_news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.AnimationUtils
-import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
 
 
     }
